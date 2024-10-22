84c8846b767fe8fc563af0d1ac1448a5505a4448
roman alekseev <roma.alekseev.90@bk.ru>
2024-10-22 09:32:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './index.scss';
+import "./index.scss";
 
 function App() {
+  let count = 0;
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
+        <h1>{count}</h1>
         <button className="minus">- Минус</button>
         <button className="plus">Плюс +</button>
       </div>
