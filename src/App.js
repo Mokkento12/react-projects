95fe4c1d0e739cc380993162c6b9187ab5a7e9bf
roman alekseev <roma.alekseev.90@bk.ru>
2024-10-22 11:54:27 +0300

    create: added correct answers logic

--- src/App.js
+++ src/App.js
@@ -27,12 +27,16 @@ const questions = [
   },
 ];
 
-function Result() {
+function Result({ correct }) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
     </div>
   );
 }
@@ -62,6 +66,7 @@ function Game({ step, question, onClickVariant }) {
 
 function App() {
   const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
 
   const question = questions[step];
 
@@ -69,12 +74,19 @@ function App() {
     console.log(step, index);
 
     setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
   };
 
   return (
     <div className="App">
-      <Game step={step} question={question} onClickVariant={onClickVariant} />
-      {/* <Result /> */}
+      {step < questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   );
 }
