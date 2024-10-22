e2b6b31bc1b802e840a8fa9f0a8bb0d5a654068c
roman alekseev <roma.alekseev.90@bk.ru>
2024-10-22 09:38:30 +0300

    deleted: readme file

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import "./index.scss";
 function App() {
   let count = 0;
 
+  const onClickPlus = () => {
+    count++;
+
+    console.log(count);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
         <h1>{count}</h1>
         <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <button onClick={onClickPlus} className="plus">
+          Плюс +
+        </button>
       </div>
     </div>
   );
