3404c20805631308ef82722df008c880de7cc4f3
roman alekseev <roma.alekseev.90@bk.ru>
2024-10-26 22:06:58 +0300

    feat: добавлена обработка ввода для конвертации валют и управление состоянием
    Добавлены переменные состояния fromPrice и toPrice для обработки значений при конвертации валют.
    Реализованы функции onChangeFromPrice и onChangeToPrice для обработки изменений значений в полях ввода.
    Настроен запрос fetch для получения обменных курсов из API и сохранения их в состоянии rates.
    Переданы новые переменные состояния и функции обработки в компоненты Block для динамического обновления на основе пользовательского ввода.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState("RUB");
+  const [toCurrency, setToCurrency] = React.useState("USD");
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(0);
+
+  const [rates, setRates] = React.useState({});
+
+  React.useEffect(() => {
+    fetch("https://api.exchangerate-api.com/v4/latest/USD")
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.rates);
+        console.log(json.rates);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Не удалось получить информацию");
+      });
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+    setToPrice(value);
+  };
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
